c22599bea7e9f54e3b96c0124bd2d5c58ee97e3a
gun <rkdrjs71@naver.com>
2024-08-20 14:35:54 +0900

    feat : 프론트 엔드 공부

--- src/components/ClientComponent.tsx
+++ src/components/ClientComponent.tsx
@@ -6,20 +6,18 @@ import styles from '@/styles/MainPage.module.css';
 export default function Home() {
     const largePRef = useRef<HTMLParagraphElement | null>(null);
     const smallPRef = useRef<HTMLParagraphElement | null>(null);
+    const buttonRef = useRef<HTMLButtonElement | null>(null);
 
     useEffect(() => {
         const largePElement = largePRef.current;
         const smallPElement = smallPRef.current;
+        const buttonElement = buttonRef.current;
 
-        const animateElement = (element: HTMLParagraphElement, delay: number, isVisible: boolean) => {
+        const animateElement = (element: HTMLElement | null, delay: number, isVisible: boolean) => {
             if (element) {
-                if (isVisible) {
-                    element.classList.remove(styles.fadeOut);
-                    element.classList.add(styles.fadeIn);
-                } else {
-                    element.classList.remove(styles.fadeIn);
-                    element.classList.add(styles.fadeOut);
-                }
+                const action = isVisible ? 'add' : 'remove';
+                element.classList[action](styles.fadeIn);
+                element.classList[action === 'add' ? 'remove' : 'add'](styles.fadeOut);
                 element.style.animationDelay = `${delay}s`;
             }
         };
@@ -27,50 +25,44 @@ export default function Home() {
         const observer = new IntersectionObserver(
             (entries) => {
                 entries.forEach((entry) => {
-                    if (entry.isIntersecting) {
-                        if (entry.target === largePElement) {
-                            animateElement(largePElement, 0, true);
-                        } else if (entry.target === smallPElement) {
-                            animateElement(smallPElement, 0.6, true);
-                        }
-                    } else {
-                        if (entry.target === largePElement) {
-                            animateElement(largePElement, 0, false);
-                        } else if (entry.target === smallPElement) {
-                            animateElement(smallPElement, 0.6, false);
-                        }
+                    const { isIntersecting, target } = entry;
+                    if (target === largePElement) {
+                        animateElement(largePElement, 0, isIntersecting);
+                    } else if (target === smallPElement) {
+                        animateElement(smallPElement, 0.6, isIntersecting);
+                    } else if (target === buttonElement) {
+                        animateElement(buttonElement, 1.0, isIntersecting);
                     }
                 });
             },
             { rootMargin: '0px', threshold: 0.1 }
         );
 
-        if (largePElement) {
-            observer.observe(largePElement);
-        }
-
-        if (smallPElement) {
-            observer.observe(smallPElement);
-        }
+        if (largePElement) observer.observe(largePElement);
+        if (smallPElement) observer.observe(smallPElement);
+        if (buttonElement) observer.observe(buttonElement);
 
         return () => {
-            if (largePElement) {
-                observer.unobserve(largePElement);
-            }
-            if (smallPElement) {
-                observer.unobserve(smallPElement);
-            }
+            if (largePElement) observer.unobserve(largePElement);
+            if (smallPElement) observer.unobserve(smallPElement);
+            if (buttonElement) observer.unobserve(buttonElement);
         };
     }, []);
 
     return (
-        <div className={`bg-blue-200 p-14`}>
-            <p ref={largePRef} className={`${styles.hidden} text-xl mb-2`}>
-                강건의 사이트 입니다.
+        <div className="bg-sky-200 p-14">
+            <p ref={largePRef} className={`${styles.hidden} text-2xl font-bold mb-3`}>
+                강건의 개발 사이트 입니다.
             </p>
-            <p ref={smallPRef} className={`${styles.hidden} text-sm`}>
-                사이트에 오신 걸 환영합니다.
+            <p ref={smallPRef} className={`${styles.hidden} text-sm mb-3`}>
+                사이트에 오신 걸 환영합니다.<br/>
+                현재 페이지 개발 중 입니다 !!
             </p>
+            <button
+                ref={buttonRef}
+                className={`${styles.hidden} px-4 py-1.5 bg-sky-500 text-white font-semibold rounded-lg shadow-md hover:bg-sky-600`}>
+                버튼
+            </button>
         </div>
     );
 }
