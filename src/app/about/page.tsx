610b7bb71ed10a051cdabfda87190a065c25981c
강건 <rkdrjs71@naver.com>
2025-02-03 23:30:31 +0900

    fix : UI FIX

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -16,6 +16,52 @@ export default function Home() {
             </div>
 
             <div className={styles.text_group}>
+                <h1>Skills</h1>
+                <div className={`${styles.child_text_group} ${styles.java}`}>
+                    <p>JAVA</p>
+                    <div className={styles.rectangle_group}>
+                        <div className={styles.rectangle}></div>
+                    </div>
+                </div>
+                <div className={`${styles.child_text_group} ${styles.js}`}>
+                    <p>JavaScript</p>
+                    <div className={styles.rectangle_group}>
+                        <div className={styles.rectangle}></div>
+                    </div>
+                </div>
+                <div className={`${styles.child_text_group} ${styles.python}`}>
+                    <p>Python</p>
+                    <div className={styles.rectangle_group}>
+                        <div className={styles.rectangle}></div>
+                    </div>
+                </div>
+                <div className={`${styles.child_text_group} ${styles.html}`}>
+                    <p>HTML</p>
+                    <div className={styles.rectangle_group}>
+                        <div className={styles.rectangle}></div>
+                    </div>
+                </div>
+                <div className={`${styles.child_text_group} ${styles.database}`}>
+                    <p>SQL / DB</p>
+                    <div className={styles.rectangle_group}>
+                        <div className={styles.rectangle}></div>
+                    </div>
+                </div>
+                <div className={`${styles.child_text_group} ${styles.cloud}`}>
+                    <p>AWS / Cloud</p>
+                    <div className={styles.rectangle_group}>
+                        <div className={styles.rectangle}></div>
+                    </div>
+                </div>
+                <div className={`${styles.child_text_group} ${styles.ai}`}>
+                    <p>AI</p>
+                    <div className={styles.rectangle_group}>
+                        <div className={styles.rectangle}></div>
+                    </div>
+                </div>
+            </div>
+
+            <div className={styles.text_group}>
                 <h1>Education</h1>
                 <div className={styles.child_text_group}>
                     <p>2024.12</p>
@@ -30,6 +76,7 @@ export default function Home() {
                     <p>조선대학교 컴퓨터 공학과 졸업</p>
                 </div>
             </div>
+
             <div className={styles.text_group}>
                 <h1>Licence</h1>
                 <div className={styles.child_text_group}>
@@ -38,72 +85,34 @@ export default function Home() {
                 </div>
                 <div className={styles.child_text_group}>
                     <p>2024.09</p>
-                    <p>SQLD</p>
+                    <p>SQL Developer</p>
                 </div>
             </div>
 
-
-            {/*<div className={styles._01049381562}>010-4938-1562</div>*/}
-            {/*<div className={styles.rkdrjs71NaverCom}>rkdrjs71@naver.com</div>*/}
 
             <div className={styles.text_group}>
-                <h1>Skills</h1>
-                <div className={`${styles.child_text_group} ${styles.java}`}>
-                    <p>JAVA</p>
-                    <div className={styles.rectangle_group}>
-                        <div className={styles.rectangle}></div>
-                    </div>
+                <h1>Career</h1>
+                <div className={styles.child_text_group}>
+                    <p>2024.12</p>
+                    <p>LG전자 DX School 1기 우수 수료생</p>
                 </div>
-                <div className={`${styles.child_text_group} ${styles.database}`}>
-                    <p>Database</p>
-                    <div className={styles.rectangle_group}>
-                        <div className={styles.rectangle}></div>
-                    </div>
+                <div className={styles.child_text_group}>
+                    <p>2024.12</p>
+                    <p>LG전자 DX School 데이터 비지니스 DX 프로젝트 우수상</p>
                 </div>
-                <div className={`${styles.child_text_group} ${styles.js}`}>
-                    <p>JS</p>
-                    <div className={styles.rectangle_group}>
-                        <div className={styles.rectangle}></div>
-                    </div>
+                <div className={styles.child_text_group}>
+                    <p>2024.11</p>
+                    <p>한국언론진흥재단 주최 2024 뉴스 빅데이터 해커톤 최우수상</p>
                 </div>
-                <div className={`${styles.child_text_group} ${styles.html}`}>
-                    <p>HTML</p>
-                    <div className={styles.rectangle_group}>
-                        <div className={styles.rectangle}></div>
-                    </div>
+                <div className={styles.child_text_group}>
+                    <p>2024.09</p>
+                    <p>LG전자 DX School 고객경험향상 CX 프로젝트 최우수상</p>
                 </div>
-                <div className={`${styles.child_text_group} ${styles.python}`}>
-                    <p>Python</p>
-                    <div className={styles.rectangle_group}>
-                        <div className={styles.rectangle}></div>
-                    </div>
-                </div>
-                <div className={`${styles.child_text_group} ${styles.cloud}`}>
-                    <p>Cloud</p>
-                    <div className={styles.rectangle_group}>
-                        <div className={styles.rectangle}></div>
-                    </div>
-                </div>
-                <div className={`${styles.child_text_group} ${styles.ai}`}>
-                    <p>AI</p>
-                    <div className={styles.rectangle_group}>
-                        <div className={styles.rectangle}></div>
-                    </div>
+                <div className={styles.child_text_group}>
+                    <p>2024.06</p>
+                    <p>Y-mart 순천점 홈페이지 구축 외주</p>
                 </div>
             </div>
-
-            {/*<div className={styles.div3}>*/}
-            {/*    <span>*/}
-            {/*        <span className={styles.div3Span}>*/}
-            {/*            성실하고 열정적인*/}
-            {/*            <br />*/}
-            {/*        </span>*/}
-            {/*        <span className={styles.div3Span2}>웹 개발자 강건</span>*/}
-            {/*    </span>*/}
-            {/*</div>*/}
-            {/*<img className={styles._3Ca5970Fc1} src="_3-ca-5970-fc-10.png" alt="Portfolio" />*/}
-            {/*<div className={styles.rectangle6}></div>*/}
-            {/*<div className={styles.div4}>소개</div>*/}
         </div>
     );
 }
